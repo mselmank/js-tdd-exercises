6091bcd6e303a46daae6f91ec9e89372e2250d56
Mati <48356632+mselmank@users.noreply.github.com>
2023-03-16 12:50:53 -0300

    [mselmank] feat (last ext folder I)

--- I.pass-tests/ES-6-practice/es6.js
+++ I.pass-tests/ES-6-practice/es6.js
@@ -1,33 +1,49 @@
 // Turn this function into an arrow function
-function arrow() {
+const arrow = () => {
   return "es6 is awesome!";
-}
+};
 
-//Use the es6 syntax for default parameter
-function defaultParameter(name) {
-  var name = name || "sam";
+// //Use the es6 syntax for default parameter
+// function defaultParameter(name) {
+//   var name = name || "sam";
+//   return name;
+// }
+//⁡⁢⁣⁣𝗨𝘀𝗲 𝘁𝗵𝗲 𝗲𝘀𝟲 𝘀𝘆𝗻𝘁𝗮𝘅 𝗳𝗼𝗿 𝗱𝗲𝗳𝗮𝘂𝗹𝘁 𝗽𝗮𝗿𝗮𝗺𝗲𝘁𝗲𝗿⁡
+const defaultParameter = (name = "sam") => {
   return name;
-}
+};
+// // Use the spread operator to combine arr1 and arr2
+// function combineArrays(arr1, arr2) {
+//   return arr1.concat(arr2);
+// }
+// ⁡⁢⁣⁣𝗨𝘀𝗲 𝘁𝗵𝗲 𝘀𝗽𝗿𝗲𝗮𝗱 𝗼𝗽𝗲𝗿𝗮𝘁𝗼𝗿 𝘁𝗼 𝗰𝗼𝗺𝗯𝗶𝗻𝗲 𝗮𝗿𝗿𝟭 𝗮𝗻𝗱 𝗮𝗿𝗿𝟮⁡
+const combineArrays = (arr1, arr2) => {
+  return [...arr1, ...arr2];
+};
 
-// Use the spread operator to combine arr1 and arr2
-function combineArrays(arr1, arr2) {
-  return arr1.concat(arr2);
-}
+// //use destructuring to return the object's cyf property
+// function destructuring(obj) {
+//   return obj.cyf;
+// }
+//⁡⁢⁣⁣𝘂𝘀𝗲 𝗱𝗲𝘀𝘁𝗿𝘂𝗰𝘁𝘂𝗿𝗶𝗻𝗴 𝘁𝗼 𝗿𝗲𝘁𝘂𝗿𝗻 𝘁𝗵𝗲 𝗼𝗯𝗷𝗲𝗰𝘁'𝘀 𝗰𝘆𝗳 𝗽𝗿𝗼𝗽𝗲𝗿𝘁𝘆⁡
+const destructuring = (obj) => {
+  const { cyf } = obj;
+  return cyf;
+};
 
-//use destructuring to return the object's cyf property
-function destructuring(obj) {
-  return obj.cyf;
-}
-
-// use template literal to return a string with the sum of a and b
-function templateString(a, b) {
-  return "The sum is equal to " + (a + b).toString();
-}
+// // use template literal to return a string with the sum of a and b
+// function templateString(a, b) {
+//   return "The sum is equal to " + (a + b).toString();
+// }
+// ⁡⁢⁣⁣𝘂𝘀𝗲 𝘁𝗲𝗺𝗽𝗹𝗮𝘁𝗲 𝗹𝗶𝘁𝗲𝗿𝗮𝗹 𝘁𝗼 𝗿𝗲𝘁𝘂𝗿𝗻 𝗮 𝘀𝘁𝗿𝗶𝗻𝗴 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝘀𝘂𝗺 𝗼𝗳 𝗮 𝗮𝗻𝗱 𝗯⁡
+const templateString = (a, b) => {
+  return `The sum is equal to ${(a + b).toString()}`;
+};
 
 module.exports = {
   arrow,
   defaultParameter,
   combineArrays,
   destructuring,
-  templateString
+  templateString,
 };
--- I.pass-tests/ES-6-practice/es6.test.js
+++ I.pass-tests/ES-6-practice/es6.test.js
@@ -3,35 +3,37 @@ const {
   defaultParameter,
   combineArrays,
   destructuring,
-  templateString
-} = require("./index");
+  templateString,
+} = require("./es6");
 
-test("function arrow is instance of a function", () => {
-  expect(arrow).toBeInstanceOf(Function);
-});
+describe("ES6 - practice", () => {
+  it("function arrow is instance of a function", () => {
+    expect(arrow).toBeInstanceOf(Function);
+  });
 
-test("function arrow returns a string of es6 is awesome", () => {
-  expect(arrow()).toEqual("es6 is awesome!");
-});
+  it("function arrow returns a string of es6 is awesome", () => {
+    expect(arrow()).toEqual("es6 is awesome!");
+  });
 
-test("defaultParameter function should return sam if no name argument passed in", () => {
-  expect(defaultParameter()).toEqual("sam");
-});
+  it("defaultParameter function should return sam if no name argument passed in", () => {
+    expect(defaultParameter()).toEqual("sam");
+  });
 
-test("defaultParameter function should return name if name argument is passed in", () => {
-  expect(defaultParameter("samatar")).toEqual("samatar");
-});
+  it("defaultParameter function should return name if name argument is passed in", () => {
+    expect(defaultParameter("samatar")).toEqual("samatar");
+  });
 
-test("combineArrays function should combine arr1 and arr2", () => {
-  expect(combineArrays(arr1, arr2)).toEqual([1, 2, 3, 4, 5, 6]);
-});
+  it("combineArrays function should combine arr1 and arr2", () => {
+    expect(combineArrays(arr1, arr2)).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 
-test("destructuring function should return cyf property of object", () => {
-  expect(destructuring(testObj)).toEqual("awesome");
-});
+  it("destructuring function should return cyf property of object", () => {
+    expect(destructuring(testObj)).toEqual("awesome");
+  });
 
-test("templateString should return a string with the sum of parameter a and b", () => {
-  expect(templateString(1, 2)).toEqual("The sum is equal to 3");
+  it("templateString should return a string with the sum of parameter a and b", () => {
+    expect(templateString(1, 2)).toEqual("The sum is equal to 3");
+  });
 });
 
 const arr1 = [1, 2, 3];
@@ -39,5 +41,5 @@ const arr2 = [4, 5, 6];
 const testObj = {
   cyf: "awesome",
   test: "100%",
-  learning: "always"
+  learning: "always",
 };
